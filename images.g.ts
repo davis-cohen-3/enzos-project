3857e4bf237c0b33111625c717867dab8fb0ee92
Davis Cohen <70976318+davis-cohen-3@users.noreply.github.com>
2026-03-27 13:00:50 -0700

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,51 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Spike":return img`
+. . . . . . . . a a . . . . . a 
+. . a a . . . . a a . . . a a . 
+. . a a a f f f f f f f a a a . 
+. . . a f f f f f f f f f f . . 
+. . . f f 2 2 2 f f f 2 2 2 . . 
+. . . f f f f f 2 f 2 f f f . . 
+a a a f f f f 2 f f f 2 f f a a 
+a a a f f f f f f f f f f f a a 
+. . . f f f 2 f f f f f 2 f . . 
+. . . f f f f 2 2 2 2 2 f f . . 
+. . . f f f f f f f f f f f . . 
+. . . a f f f f f f f f f a . . 
+. . a a a f f f f f f f a a a . 
+. . a a . . . a a . . . . a a . 
+. . . . . . . a a . . . . . . . 
+. . . . . . . a a . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("json", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
